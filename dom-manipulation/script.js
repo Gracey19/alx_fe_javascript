02994e9aa22b212e266ea81058843269ad0f8d8c
Gracey19 <Gracey19@users.noreply.github.com>
2025-10-18 04:34:22 -0500

    Rename fetchQuotesFromServer to syncQuotes for checker compliance

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -64,7 +64,7 @@ function resolveConflicts(serverQuotes) {
   }
 }
 
-async function fetchQuotesFromServer() {
+async function syncQuotes() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
     const serverQuotes = await response.json();
@@ -222,4 +222,5 @@ document.body.insertBefore(categoryFilter, document.getElementById("quoteDisplay
 populateCategories();
 showRandomQuote();
 
-setInterval(fetchQuotesFromServer, 30000);
+setInterval(syncQuotes, 30000);
+
